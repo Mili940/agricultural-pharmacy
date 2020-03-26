588bd2292a9696564c3d45e7d8005e0d78342500
Mili940 <miliup@live.com>
2020-03-26 14:09:06 +0100

    Add weather widget

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,10 @@
         </nav>
 
         <main class="py-4 mx-auto w-2/3">
+            <a class="weatherwidget-io" href="https://forecast7.com/sr/45d0119d37/kuzmin/" data-label_1="KUZMIN" data-label_2="WEATHER" data-theme="orange" >KUZMIN WEATHER</a>
+            <script>
+                !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+            </script>
             @yield('content')
         </main>
     </div>
